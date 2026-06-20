faced1de6b3431d916fa332182a20da358fa89c7
Vanessa Victorino <vanessaf.victorino@yahoo.com>
2026-06-19 20:39:32 -0500

    fix: scope InfoPage scroll behavior to responsive layout

--- src/pages/InfoAmenities.jsx
+++ src/pages/InfoAmenities.jsx
@@ -1,9 +1,9 @@
 function InfoAmenities() {
   return (
-<aside
-  className="p-5 w-100 h-100 overflow-auto info-panel"
-  style={{ background: "#d1ecf1" }}
->
+    <aside
+      className="p-5 w-100 h-100 info-panel"
+      style={{ background: "#d1ecf1" }}
+    >
       <h1 className="mb-4">Amelia’s Bed & Breakfast Inn</h1>
 
       <h3>Visitor’s Guide</h3>
